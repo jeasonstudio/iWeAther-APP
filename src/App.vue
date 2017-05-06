0b69a6bc16a06b17386715052be63b79f921a768
赵吉彤 <zjtong3576@sina.com>
2017-05-07 01:27:36 +0800

    finish add com swiper

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,59 @@
 <style lang="stylus" src="stylus"></style>
 <style lang="stylus" scoped>
     @import '~stylus/vars'
-    @import '~stylus/mixins'
+    // @import '~stylus/mixins'
     #app
-        background-color app-color
-        display flex
-        flex-direction column
-        min-height 100vh
-        // max-width 1024px
-        margin 0 auto
-        shadow 4
-
-        footer
-            padding 10px
-        main
-            font-size 1.3rem
-            flex 1 0 auto
-        footer
-            background-color white
-            font-size 0.8rem
-            color #666
-            text-align center
-            a 
-                color #666
-
-    .route-enter-active
-    .route-leave-active
-        transition opacity transition-time
-    .route-enter
-    .route-leave-to
-        opacity 0
+        height 100%
+        background-color #fff
+        .swiper
+            height 100%
+            .swiper-roll
+                height 100%
+            .weather-info
+                color red
+            .weather-details
+                display flex
+                flex-flow column nowrap
+                color greyBlack-color
+                footer
+                    padding 10px
+                    font-size 1rem
+                    flex 1 0 auto
+                    display flex
+                    justify-content center
+                    align-items flex-end
+                    a
+                        color greyBlack-color
+        
 </style>
 <template>
     <div class="app" id="app">
-        <main>
-            <transition name="route" mode="out-in">
-                <router-view @view="view"></router-view>
-            </transition>
-        </main>
-        <footer>
-            Copyright &copy; 2015-2017 <a href="https://github.com/jeasonstudio">JeasonStudio</a>
-        </footer>
+        <swiper ref="swiper" direction="vertical" :paginationVisible="false" :mousewheel-control="true" :performance-mode="false" :pagination-visible="true" :pagination-clickable="true" :loop="false" @slide-change-start="onSlideChangeStart" @slide-change-end="onSlideChangeEnd">
+            <section class="swiper-roll weather-info">Page 0</section>
+            <section class="swiper-roll weather-details">
+                <div>asdasdasdas</div>
+                <footer>
+                    Copyright &copy; 2015-2017 <a href="https://github.com/jeasonstudio">JeasonStudio</a>
+                </footer>
+            </section>
+        </swiper>
+        <!--<main>
+                    <transition name="route" mode="out-in">
+                        <router-view @view="view"></router-view>
+                    </transition>
+                </main>-->
     </div>
 </template>
 <script>
+import Swiper from './components/vue-swiper'
 export default {
     name: 'App',
+    data() {
+        return {
+
+        }
+    },
+    components: { Swiper },
     methods: {
         view({ title, description, keywords }) {
             if (title) {
@@ -57,6 +65,29 @@ export default {
             if (keywords) {
                 document.head.querySelector('meta[name=keywords]').content = keywords
             }
+        },
+        onSlideChangeStart: function (currentPage) {
+            console.log('onSlideChangeStart', currentPage);
+        },
+        onSlideChangeEnd: function (currentPage) {
+            console.log('onSlideChangeEnd', currentPage);
+        },
+        onSlideRevertStart: function (currentPage) {
+            console.log('onSlideRevertStart', currentPage);
+        },
+        onSlideRevertEnd: function (currentPage) {
+            console.log('onSlideRevertEnd', currentPage);
+        },
+        onSliderMove: function (offset) {
+            console.log('onSliderMove', offset);
+        },
+        prependSlide: function (slideText) {
+            this.slides.unshift(slideText);
+        },
+        appendSlide: function (slideText) {
+            this.slides.push(slideText);
+        },
+        removeSlide: function () {
         }
     }
 }
--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ import router from './router/index'
 import axios from 'axios'
 import echarts from 'echarts'
 
-axios.defaults.baseURL = 'https://free-api.heweather.com/v5';
-axios.defaults.params = {
-    city: '127.0.0.1',  //根据本机 ip 获取城市
-    key: '6fcd1f55dc9d4c059b6ae73ec24f2ce2' // 生成的 key
-};
+axios.defaults = {
+    baseURL: 'https://free-api.heweather.com/v5',
+    params: {
+        city: '127.0.0.1', //根据本机 ip 获取城市
+        key: '6fcd1f55dc9d4c059b6ae73ec24f2ce2' // 生成的 key
+    }
+}
 
 Vue.prototype.$http = axios;
 Vue.prototype.$echarts = echarts;
